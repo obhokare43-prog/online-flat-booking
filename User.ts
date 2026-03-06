cd7df0d12bf0b58a62a09b68666cf34867f7d0fa
obhokare43-prog <obhokare43@gmail.com>
2026-03-06 23:23:27 +0530

    Models
    UserModels

---
+++ User.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export interface IUser extends mongoose.Document {
+  name: string;
+  email: string;
+  password?: string;
+  role: 'user' | 'admin';
+}
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+}, { timestamps: true });
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
